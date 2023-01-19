50ebc7b48e8fa590377e4b9d0cd91176a23f52da
aslogvinov <aslogvinov@sbercloud.ru>
2023-01-19 20:01:07 +0300

    Added searching with trigram similarity

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,7 +4,7 @@ from django.core.mail import send_mail
 from django.views.decorators import http
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 
 from taggit.models import Tag
 
@@ -89,10 +89,18 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title',  weight='A') + SearchVector('body', weight='B')
-            search_query = SearchQuery(query)
+            # uncomment lines to add simple fulltext search instead of trigram similarity:
+            # search_vector = SearchVector('title',  weight='A') + SearchVector('body', weight='B')
+            # search_query = SearchQuery(query)
+            # results = Post.published.annotate(
+            #     search=search_vector,
+            #     rank=SearchRank(search_vector, search_query)
+            # ).filter(rank__gte=0.3).order_by('-rank')
+
+            # execute the following command to install the pg_trgm extension:
+            # CREATE EXTENSION pg_trgm;
             results = Post.published.annotate(
-                search=search_vector, rank=SearchRank(search_vector, search_query)
-            ).filter(rank__gte=0.3).order_by('-rank')
+                            similarity=TrigramSimilarity('title', query),
+                        ).filter(similarity__gt=0.1).order_by('-similarity')
     return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
 
